81ab781bd75746dcaf385046307ac211decd6ac6
spamegg <spamegg@pm.me>
2025-04-27 13:37:22 +0300

    replace GFunction's code with GArray2DFunction

--- cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
+++ cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
@@ -1,17 +1,21 @@
 package io.computenode.cyfra.runtime
 
 import io.computenode.cyfra.dsl.Algebra.FromExpr
-import io.computenode.cyfra.dsl.Value.*
-import io.computenode.cyfra.vulkan.compute.{ComputePipeline, Shader}
-import io.computenode.cyfra.*
-import io.computenode.cyfra.dsl.* 
+import io.computenode.cyfra.dsl.{GArray2D, GStruct, GStructSchema, Value}
+import io.computenode.cyfra.dsl.Value.Int32
+import io.computenode.cyfra.vulkan.compute.ComputePipeline
 import izumi.reflect.Tag
 
-import scala.deriving.Mirror
-
-case class GFunction[H <: Value : Tag: FromExpr, R <: Value : Tag : FromExpr](fn: H => R)(implicit context: GContext){
+case class GFunction[
+  G <: GStruct[G] : GStructSchema : Tag, 
+  H <: Value : Tag : FromExpr, 
+  R <: Value : Tag : FromExpr
+](
+  width: Int,
+  height: Int,
+  fn: (G, (Int32, Int32), GArray2D[H]) => R
+)(implicit context: GContext){
   def arrayInputs: List[Tag[_]] = List(summon[Tag[H]])
   def arrayOutputs: List[Tag[_]] = List(summon[Tag[R]])
   val pipeline: ComputePipeline = context.compile(this)
 }
-
