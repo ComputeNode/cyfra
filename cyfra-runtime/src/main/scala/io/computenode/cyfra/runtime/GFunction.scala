d5220fb84e5f1aa1af7abd398bea75056b1b960d
spamegg <spamegg@pm.me>
2025-04-28 11:49:07 +0300

    REDESIGN GFunction

--- cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
+++ cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
@@ -1,7 +1,7 @@
 package io.computenode.cyfra.runtime
 
 import io.computenode.cyfra.dsl.Algebra.FromExpr
-import io.computenode.cyfra.dsl.{GArray2D, GStruct, GStructSchema, Value}
+import io.computenode.cyfra.dsl.{GArray, GStruct, GStructSchema, Value}
 import io.computenode.cyfra.dsl.Value.Int32
 import io.computenode.cyfra.vulkan.compute.ComputePipeline
 import izumi.reflect.Tag
@@ -10,11 +10,7 @@ case class GFunction[
   G <: GStruct[G] : GStructSchema : Tag, 
   H <: Value : Tag : FromExpr, 
   R <: Value : Tag : FromExpr
-](
-  width: Int,
-  height: Int,
-  fn: (G, (Int32, Int32), GArray2D[H]) => R
-)(implicit context: GContext){
+](fn: (G, Int32, GArray[H]) => R)(implicit context: GContext){
   def arrayInputs: List[Tag[_]] = List(summon[Tag[H]])
   def arrayOutputs: List[Tag[_]] = List(summon[Tag[R]])
   val pipeline: ComputePipeline = context.compile(this)
@@ -22,12 +18,9 @@ case class GFunction[
 
 object GFunction:
   def apply[
-    H <: Value : Tag : FromExpr, 
+    H <: Value : Tag : FromExpr,
     R <: Value : Tag : FromExpr
   ](fn: H => R)(using context: GContext) = 
     new GFunction[GStruct.Empty, H, R](
-      ???,
-      ???,
-      (_, xy: (Int32, Int32), gArray: GArray2D[H])
-        => fn(gArray.at(xy._1, xy._2))
+      (_, index: Int32, gArray: GArray[H]) => fn(gArray.at(index))
     )
