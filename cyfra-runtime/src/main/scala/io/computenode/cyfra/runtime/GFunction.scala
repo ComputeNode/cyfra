489e69983e53e155e9a461b838c4182f32382c1e
spamegg <spamegg@pm.me>
2025-04-29 09:11:49 +0300

    remove comment

--- cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
+++ cyfra-runtime/src/main/scala/io/computenode/cyfra/runtime/GFunction.scala
@@ -34,6 +34,6 @@ object GFunction:
       (g: G, index: Int32, a: GArray[H]) =>
         val x: Int32 = index / width
         val y: Int32 = index mod width
-        val arr = GArray2D(width, width, a) // fix this! GArray2D should only have width.
+        val arr = GArray2D(width, width, a)
         fn(g, (x, y), arr)
     )
