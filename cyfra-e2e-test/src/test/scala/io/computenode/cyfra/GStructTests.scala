3a06e603e8347d893b55622d4bca8d02faae37d1
spamegg <spamegg@pm.me>
2025-05-27 13:52:24 +0300

    trying UniformContext - test still fails

--- cyfra-e2e-test/src/test/scala/io/computenode/cyfra/GStructTests.scala
+++ cyfra-e2e-test/src/test/scala/io/computenode/cyfra/GStructTests.scala
@@ -9,18 +9,20 @@ class GStructE2eTest extends munit.FunSuite:
 
   test("custom GStruct"):
     case class Custom(f: Float32, v: Vec4[Float32]) extends GStruct[Custom]
-    val gf: GFunction[Custom, Float32, Float32] = GFunction:
-      (custom, index, gArray) => custom.f
 
-    val inArr = (0 to 255).map(_.toFloat).toArray
-    val gmem = FloatMem(inArr)
-    val result = gmem.map(gf).asInstanceOf[FloatMem].toArray
+    UniformContext.withUniform(Custom(2f, (1f, 2f, 3f, 4f))):
+      val gf: GFunction[Custom, Float32, Float32] = GFunction:
+        case (Custom(f, v), index, gArray) => v.*(f).dot(v) + gArray.at(index) * f
 
-    val expected = inArr
-    result
-      .zip(expected)
-      .foreach: (res, exp) =>
-        assert(Math.abs(res - exp) < 0.001f, s"Expected $exp but got $res")
+      val inArr = (0 to 255).map(_.toFloat).toArray
+      val gmem = FloatMem(inArr)
+      val result = gmem.map(gf).asInstanceOf[FloatMem].toArray
+
+      val expected = inArr.map(f => 2f * f + 60f)
+      result
+        .zip(expected)
+        .foreach: (res, exp) =>
+          assert(Math.abs(res - exp) < 0.001f, s"Expected $exp but got $res")
 
   test("GStruct of GStructs"):
     val gf: GFunction[GStruct.Empty, Float32, Float32] = GFunction: f =>
