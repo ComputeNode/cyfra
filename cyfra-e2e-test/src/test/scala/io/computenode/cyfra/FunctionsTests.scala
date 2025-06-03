b2da97df4a1e1148dee7c95858942f2ae12a638b
spamegg <spamegg@pm.me>
2025-06-03 12:19:57 +0300

    add tests for missing ext functions

--- cyfra-e2e-test/src/test/scala/io/computenode/cyfra/FunctionsTests.scala
+++ cyfra-e2e-test/src/test/scala/io/computenode/cyfra/FunctionsTests.scala
@@ -3,6 +3,7 @@ package io.computenode.cyfra.e2e
 import io.computenode.cyfra.runtime.*, mem.*
 import io.computenode.cyfra.dsl.{*, given}
 import GStruct.Empty.given
+import GMem.fRGBA
 
 class FunctionsE2eTest extends munit.FunSuite:
   given gc: GContext = GContext()
@@ -27,18 +28,55 @@ class FunctionsE2eTest extends munit.FunSuite:
       .foreach: (res, exp) =>
         assert(Math.abs(res - exp) < 0.01f, s"Expected $exp but got $res")
 
-  test("smoothstep clamp mix reflect refract normalize"): // STUB
+  test("smoothstep clamp mix reflect refract normalize"):
     val gf: GFunction[GStruct.Empty, Float32, Float32] = GFunction: f =>
-      0f
+      val f1 = smoothstep(1.2f, 3.4f, f)
+      val f2 = mix(f, f + 1f, f1)
+      val v1: Vec4[Float32] = (f1, f2, f1 + 3.4f, f2 - 1.2f)
+      val v2: Vec4[Float32] = (f1 * 3.4f, f1, f2, f2 / 1.2f)
+      val v3 = mix(v1, v2, f2)
+      val v4 = reflect(v3, v2)
+      val v5 = refract(normalize(v3), normalize(v4), f2)
+      v5.dot(v1)
 
     val inArr = (0 to 255).map(_.toFloat).toArray
     val gmem = FloatMem(inArr)
     val result = gmem.map(gf).asInstanceOf[FloatMem].toArray
 
+    extension (f: fRGBA)
+      def neg: fRGBA = (-f._1, -f._2, -f._3, -f._4)
+      def add(that: fRGBA) = (f._1 + that._1, f._2 + that._2, f._3 + that._3, f._4 + that._4)
+      def sub(that: fRGBA) = f.add(that.neg)
+      def scl(s: Float) = (f._1 * s, f._2 * s, f._3 * s, f._4 * s)
+      def div(s: Float): fRGBA = (f._1 / s, f._2 / s, f._3 / s, f._4 / s)
+      def pro(that: fRGBA) = f._1 * that._1 + f._2 * that._2 + f._3 * that._3 + f._4 * that._4
+      def norm: Float = math.sqrt(f.pro(f)).toFloat
+      def normalized: fRGBA = f.div(f.norm)
+      def reflected(n: fRGBA): fRGBA = f.sub(n.scl(2 * n.pro(f)))
+      def refracted(n: fRGBA, eta: Float): fRGBA = // assumes f, n are normalized
+        val dotted = n.pro(f)
+        val k = 1.0f - eta * eta * (1.0f - dotted * dotted)
+        if k < 0.0 then (0f, 0f, 0f, 0f)
+        else f.scl(eta).sub(n.scl(eta * dotted + math.sqrt(k).toFloat)) 
+
+    def floatMix(a: Float, b: Float, t: Float): Float = a * (1 - t) + b * t
+    def vecMix(a: fRGBA, b: fRGBA, t: Float): fRGBA = (a.scl(1 - t)).add(b.scl(t))
+    def scalaClamp(f: Float, from: Float, to: Float) = math.min(math.max(f, from), to)
+    def scalaSmooth(e0: Float, e1: Float, x: Float): Float =
+      val t = scalaClamp((x - e0) / (e1 - e0), 0f, 1f)
+      t * t * (3 - 2 * t)
+
     val expected = inArr.map: f =>
-      0f
+      val f1 = scalaSmooth(1.2f, 3.4f, f)
+      val f2 = floatMix(f, f + 1f, f1)
+      val v1 = (f1, f2, f1 + 3.4f, f2 - 1.2f)
+      val v2 = (f1 * 3.4f, f1, f2, f2 / 1.2f)
+      val v3 = vecMix(v1, v2, f2)
+      val v4 = v3.reflected(v2)
+      val v5 = v3.normalized.refracted(v4.normalized, f2)
+      v5.pro(v1)
 
     result
       .zip(expected)
       .foreach: (res, exp) =>
-        assert(Math.abs(res - exp) < 0.01f, s"Expected $exp but got $res")
+        assert(Math.abs(res - exp) < 0.001f, s"Expected $exp but got $res")
