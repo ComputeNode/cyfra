55727304cba609598904b579a99142104b301874
Szymon <szymonrodant@gmail.com>
2025-07-06 14:30:44 +0200

    changed program

--- cyfra-core/src/main/scala/io/computenode/cyfra/core/GProgram.scala
+++ cyfra-core/src/main/scala/io/computenode/cyfra/core/GProgram.scala
@@ -13,15 +13,28 @@ import io.computenode.cyfra.dsl.struct.GStruct
 import io.computenode.cyfra.dsl.struct.GStruct.Empty
 import izumi.reflect.Tag
 
-class GProgram[Params, L <: Layout: LayoutStruct] private (
-  val body: L => GIO[?],
-  val layout: InitProgramLayout => Params => L,
-  val dispatch: (L, Params) => ProgramDispatch,
-  val workgroupSize: WorkDimensions,
-) extends GExecution[Params, L, L]:
-  private[cyfra] def layoutStruct: LayoutStruct[L] = summon[LayoutStruct[L]]
+sealed trait GProgram[Params, L <: Layout: LayoutStruct] extends GExecution[Params, L, L]:
+  val layout: InitProgramLayout => Params => L
+  val dispatch: (L, Params) => ProgramDispatch
+  val workgroupSize: WorkDimensions
 
 object GProgram:
+
+  class GioProgram[Params, L <: Layout : LayoutStruct](
+    val body: L => GIO[?],
+    val layout: InitProgramLayout => Params => L,
+    val dispatch: (L, Params) => ProgramDispatch,
+    val workgroupSize: WorkDimensions,
+  ) extends GProgram[Params, L]:
+    private[cyfra] def layoutStruct: LayoutStruct[L] = summon[LayoutStruct[L]]
+
+  class SpirvProgram[Params, L <: Layout : LayoutStruct](
+    val code: ByteBuffer,
+    val layout: InitProgramLayout => Params => L,
+    val dispatch: (L, Params) => ProgramDispatch,
+    val workgroupSize: WorkDimensions,
+  ) extends GProgram[Params, L]:
+    private[cyfra] def layoutStruct: LayoutStruct[L] = summon[LayoutStruct[L]]
 
   type WorkDimensions = (Int, Int, Int)
 
@@ -54,5 +67,5 @@ object GProgram:
     dispatch: (L, Params) => ProgramDispatch,
     workgroupSize: WorkDimensions = (128, 1, 1),
   )(body: L => GIO[?]): GProgram[Params, L] =
-    new GProgram(body, s => layout(using s), dispatch, workgroupSize)
+    new GioProgram[Params, L](body, s => layout(using s), dispatch, workgroupSize)
 
