9e210ddf3e1dc0b823f956b0c0ca4eb03541b053
MarconZet <25779550+MarconZet@users.noreply.github.com>
2026-01-25 22:51:16 +0100

    done

--- cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/Focus.scala
+++ cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/Focus.scala
@@ -26,8 +26,160 @@ object Focus:
 
   private def focusImpl[From: Type, To: Type](from: Expr[Focus[From]], lambda: Expr[FocusContext ?=> From => To])(using quotes: Quotes): Expr[Focus[To]] =
     import quotes.reflect.*
-    given Printer[Tree] = Printer.TreeCode
 
-    report.info(from.show)
-    report.info(lambda.show)
-    '{ null.asInstanceOf[Focus[To]] }
+    // Represents an access step in the focus path
+    enum AccessStep:
+      case TupleElement(index: Int, parentType: TypeRepr, elementType: TypeRepr)
+      case ArrayConstant(index: Int, elementType: TypeRepr)
+      case ArrayDynamic(indexExpr: Term, indexType: TypeRepr, elementType: TypeRepr)
+
+    // Extract lambda body - handles context function wrapping
+    def extractLambdaBody(term: Term): Term = term match
+      case Inlined(_, _, body) => extractLambdaBody(body)
+      case Block(List(DefDef(_, _, _, Some(body))), _) => extractLambdaBody(body)
+      case Block(Nil, body) => extractLambdaBody(body)
+      case Lambda(_, body) => body
+      case _ => term
+
+    // Get the tuple element index from method name like "_1", "_2", etc.
+    def tupleElementIndex(name: String): Option[Int] =
+      if name.startsWith("_") then name.drop(1).toIntOption else None
+
+    // Recursively collect access steps from the expression tree
+    // Returns (steps from outer to inner, the parameter identifier)
+    def collectSteps(term: Term): (List[AccessStep], Term) = term match
+      // Handle inlined expressions
+      case Inlined(_, _, body) =>
+        collectSteps(body)
+
+      // Tuple element access: expr._N
+      case Select(qualifier, name) if tupleElementIndex(name).isDefined =>
+        val index = tupleElementIndex(name).get
+        val (innerSteps, param) = collectSteps(qualifier)
+        val step = AccessStep.TupleElement(index, qualifier.tpe.widen, term.tpe.widen)
+        (innerSteps :+ step, param)
+
+      // Extension method array access with constant Int: context.at[Elem](qualifier)(constIndex)(evidence)
+      // Tree: Apply(Apply(Apply(TypeApply(Select(context, "at"), List(elemType)), List(qualifier)), List(index)), List(evidence))
+      case Apply(Apply(Apply(TypeApply(Select(_, "at"), List(elemTypeTree)), List(qualifier)), List(indexTerm)), _)
+          if indexTerm.tpe.widen <:< TypeRepr.of[Int] =>
+        indexTerm match
+          case Literal(IntConstant(idx)) =>
+            val (innerSteps, param) = collectSteps(qualifier)
+            val elemType = elemTypeTree.tpe.widen
+            val step = AccessStep.ArrayConstant(idx, elemType)
+            (innerSteps :+ step, param)
+          case _ =>
+            report.errorAndAbort(s"Expected constant Int index in at(), got: ${indexTerm.show}")
+
+      // Extension method array access with dynamic IntegerType: context.at[Elem, I](qualifier)(index)(evidences)
+      // Tree: Apply(Apply(Apply(TypeApply(Select(context, "at"), List(elemType, indexType)), List(qualifier)), List(index)), List(evidences))
+      case Apply(Apply(Apply(TypeApply(Select(_, "at"), List(elemTypeTree, _)), List(qualifier)), List(indexTerm)), _) =>
+        val (innerSteps, param) = collectSteps(qualifier)
+        val elemType = elemTypeTree.tpe.widen
+        val step = AccessStep.ArrayDynamic(indexTerm, indexTerm.tpe.widen, elemType)
+        (innerSteps :+ step, param)
+
+      // Base case: the lambda parameter
+      case ident: Ident =>
+        (Nil, ident)
+
+      case other =>
+        report.errorAndAbort(s"Unsupported focus expression: ${other.show}\nTree: ${other}")
+
+    // Build the Focus expression from collected steps
+    def buildFocusExpr(steps: List[AccessStep], currentFocus: Term, currentType: TypeRepr): Term =
+      steps match
+        case Nil => currentFocus
+        case step :: rest =>
+          step match
+            case AccessStep.TupleElement(index, parentType, elementType) =>
+              val parentTypeTree = TypeTree.of(using parentType.asType)
+              val elementTypeTree = TypeTree.of(using elementType.asType)
+
+              // Find Value instances for parent and element types
+              val parentValue = Implicits.search(TypeRepr.of[Value].appliedTo(parentType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${parentType.show}")
+
+              val elementValue = Implicits.search(TypeRepr.of[Value].appliedTo(elementType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${elementType.show}")
+
+              val focusConstantType = TypeRepr.of[FocusConstant].appliedTo(List(parentType, elementType))
+              val focusConstantCompanion = Ref(Symbol.requiredModule("io.computenode.cyfra.core.expression.FocusConstant"))
+
+              val newFocus = Apply(
+                Apply(
+                  TypeApply(
+                    Select.unique(focusConstantCompanion, "apply"),
+                    List(parentTypeTree, elementTypeTree)
+                  ),
+                  List(currentFocus, Literal(IntConstant(index)))
+                ),
+                List(parentValue, elementValue)
+              )
+              buildFocusExpr(rest, newFocus, elementType)
+
+            case AccessStep.ArrayConstant(index, elementType) =>
+              val parentType = TypeRepr.of[RuntimeArray].appliedTo(elementType)
+              val parentTypeTree = TypeTree.of(using parentType.asType)
+              val elementTypeTree = TypeTree.of(using elementType.asType)
+
+              val parentValue = Implicits.search(TypeRepr.of[Value].appliedTo(parentType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${parentType.show}")
+
+              val elementValue = Implicits.search(TypeRepr.of[Value].appliedTo(elementType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${elementType.show}")
+
+              val focusConstantCompanion = Ref(Symbol.requiredModule("io.computenode.cyfra.core.expression.FocusConstant"))
+
+              val newFocus = Apply(
+                Apply(
+                  TypeApply(
+                    Select.unique(focusConstantCompanion, "apply"),
+                    List(parentTypeTree, elementTypeTree)
+                  ),
+                  List(currentFocus, Literal(IntConstant(index)))
+                ),
+                List(parentValue, elementValue)
+              )
+              buildFocusExpr(rest, newFocus, elementType)
+
+            case AccessStep.ArrayDynamic(indexExpr, indexType, elementType) =>
+              val parentType = TypeRepr.of[RuntimeArray].appliedTo(elementType)
+              val parentTypeTree = TypeTree.of(using parentType.asType)
+              val elementTypeTree = TypeTree.of(using elementType.asType)
+
+              val parentValue = Implicits.search(TypeRepr.of[Value].appliedTo(parentType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${parentType.show}")
+
+              val elementValue = Implicits.search(TypeRepr.of[Value].appliedTo(elementType)) match
+                case success: ImplicitSearchSuccess => success.tree
+                case _ => report.errorAndAbort(s"Could not find Value instance for ${elementType.show}")
+
+              val focusDynamicCompanion = Ref(Symbol.requiredModule("io.computenode.cyfra.core.expression.FocusDynamic"))
+
+              // Cast the index expression to IntegerType
+              val indexAsIntegerType = indexExpr.asExprOf[IntegerType].asTerm
+
+              val newFocus = Apply(
+                Apply(
+                  TypeApply(
+                    Select.unique(focusDynamicCompanion, "apply"),
+                    List(parentTypeTree, elementTypeTree)
+                  ),
+                  List(currentFocus, indexAsIntegerType)
+                ),
+                List(parentValue, elementValue)
+              )
+              buildFocusExpr(rest, newFocus, elementType)
+
+    val lambdaBody = extractLambdaBody(lambda.asTerm)
+    val (steps, _) = collectSteps(lambdaBody)
+
+    val result = buildFocusExpr(steps, from.asTerm, TypeRepr.of[From])
+    result.asExprOf[Focus[To]]
---
+++ cyfra-core/src/test/scala/io/computenode/cyfra/core/expression/FocusTest.scala
@@ -0,0 +1,70 @@
+package io.computenode.cyfra.core.expression
+
+import io.computenode.cyfra.core.expression.Focus.*
+import io.computenode.cyfra.core.expression.types.*
+import io.computenode.cyfra.core.expression.types.given
+import izumi.reflect.{Tag, TagK}
+
+class FocusTest extends munit.FunSuite:
+
+  private type Inner = (Float32, UInt32)
+  private type Struct = (Int32, Inner)
+
+  private def eNa[T: Value](block: ExpressionBlock[?], exp: Expression[T]): T =
+    exp.v.extract(block.add(exp))
+
+  private given Value[Inner] = new Value:
+    protected def extractUnsafe(ir: ExpressionBlock[Inner]): Inner =
+      val a = Expression.Composite[Inner, 0](ir.result, 0)
+      val b = Expression.Composite[Inner, 1](ir.result, 1)
+      (eNa(ir, a), eNa(ir, b))
+
+    def tag: Tag[Inner] = Tag[Inner]
+    def baseTag: Option[TagK[?]] = Some(Tag[Tuple2].asInstanceOf[TagK[?]])
+    def composite: List[Value[?]] = List(Value[Float32], Value[UInt32])
+
+  private given Value[Struct] = new Value:
+    protected def extractUnsafe(ir: ExpressionBlock[Struct]): Struct =
+      val a = Expression.Composite[Struct, 0](ir.result, 0)
+      val b = Expression.Composite[Struct, 1](ir.result, 1)
+      (eNa(ir, a), b.v.extract(ir.add(b)))
+
+    def tag: Tag[Struct] = Tag[Struct]
+    def baseTag: Option[TagK[?]] = Some(Tag[Tuple2].asInstanceOf[TagK[?]])
+    def composite: List[Value[?]] = List(Value[Int32], Value[Inner])
+
+  test("focus on nested tuple elements"):
+    val v1: Var[Struct] = new Var()
+
+    val result = v1.focus(_._2._1)
+    val expected: Focus[Float32] = FocusConstant[Inner, Float32](FocusConstant[Struct, Inner](v1, 2), 1)
+
+    assertEquals(result, expected)
+
+  test("focus with an alternative syntax"):
+    val v1: Var[Struct] = new Var()
+
+    val result1 = v1.focus(x => x._2._1)
+    val result2 = v1.focus: x =>
+      x._2._1
+    val expected: Focus[Float32] = FocusConstant[Inner, Float32](FocusConstant[Struct, Inner](v1, 2), 1)
+
+    assertEquals(result1, expected)
+    assertEquals(result2, expected)
+
+  test("focus on RuntimeArray with constant index and tuple element"):
+    val v2: Var[RuntimeArray[Inner]] = new Var()
+
+    val result = v2.focus(_.at(10)._2)
+    val expected: Focus[UInt32] = FocusConstant[Inner, UInt32](FocusConstant[RuntimeArray[Inner], Inner](v2, 10), 2)
+
+    assertEquals(result, expected)
+
+  test("focus on RuntimeArray with dynamic IntegerType index"):
+    val v2: Var[RuntimeArray[Inner]] = new Var()
+    val i: Int32 = Int32(9)
+
+    val result = v2.focus(_.at(i))
+    val expected: Focus[Inner] = FocusDynamic[RuntimeArray[Inner], Inner](v2, i)
+
+    assertEquals(result, expected)
