d3a49943a61e27436913f375d3a6aa00a65fa0c0
MarconZet <25779550+MarconZet@users.noreply.github.com>
2026-04-12 21:43:22 +0200

    ack

---
+++ cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/types/Scalar.scala
@@ -0,0 +1,66 @@
+package io.computenode.cyfra.core.expression.types
+
+import io.computenode.cyfra.core.expression.*
+import izumi.reflect.Tag
+
+private[types] def const[A: Value](value: Any): A =
+  Value[A].extract(ExpressionBlock(Expression.Constant[A](value)))
+
+sealed trait Scalar
+
+abstract class Bool extends Scalar
+object Bool:
+  def apply(value: Boolean): Bool = const(value)
+  given Value.Scalar[Bool] with
+    protected def extractUnsafe(ir: ExpressionBlock[Bool]): Bool = new BoolImpl(ir)
+    def tag: Tag[Bool] = Tag[Bool]
+
+sealed trait NumericalType extends Scalar
+sealed trait NegativeType extends NumericalType
+
+sealed trait FloatType extends NegativeType
+abstract class Float16 extends FloatType
+object Float16:
+  def apply(value: Float): Float16 = const(value)
+  given Value.Scalar[Float16] with
+    protected def extractUnsafe(ir: ExpressionBlock[Float16]): Float16 = new Float16Impl(ir)
+    def tag: Tag[Float16] = Tag[Float16]
+
+abstract class Float32 extends FloatType
+object Float32:
+  def apply(value: Float): Float32 = const(value)
+  given Value.Scalar[Float32] with
+    protected def extractUnsafe(ir: ExpressionBlock[Float32]): Float32 = new Float32Impl(ir)
+    def tag: Tag[Float32] = Tag[Float32]
+
+sealed trait IntegerType extends NumericalType
+
+sealed trait SignedIntType extends IntegerType with NegativeType
+abstract class Int16 extends SignedIntType
+object Int16:
+  def apply(value: Int): Int16 = const(value)
+  given Value.Scalar[Int16] with
+    protected def extractUnsafe(ir: ExpressionBlock[Int16]): Int16 = new Int16Impl(ir)
+    def tag: Tag[Int16] = Tag[Int16]
+
+abstract class Int32 extends SignedIntType
+object Int32:
+  def apply(value: Int): Int32 = const(value)
+  given Value.Scalar[Int32] with
+    protected def extractUnsafe(ir: ExpressionBlock[Int32]): Int32 = new Int32Impl(ir)
+    def tag: Tag[Int32] = Tag[Int32]
+
+sealed trait UnsignedIntType extends IntegerType
+abstract class UInt16 extends UnsignedIntType
+object UInt16:
+  def apply(value: Int): UInt16 = const(value)
+  given Value.Scalar[UInt16] with
+    protected def extractUnsafe(ir: ExpressionBlock[UInt16]): UInt16 = new UInt16Impl(ir)
+    def tag: Tag[UInt16] = Tag[UInt16]
+
+abstract class UInt32 extends UnsignedIntType
+object UInt32:
+  def apply(value: Int): UInt32 = const(value)
+  given Value.Scalar[UInt32] with
+    protected def extractUnsafe(ir: ExpressionBlock[UInt32]): UInt32 = new UInt32Impl(ir)
+    def tag: Tag[UInt32] = Tag[UInt32]
