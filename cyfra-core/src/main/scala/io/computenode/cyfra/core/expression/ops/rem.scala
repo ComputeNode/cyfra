4c1c38a13920821c778dbf1a4e50a29c79a04ef8
MarconZet <25779550+MarconZet@users.noreply.github.com>
2026-04-13 13:48:39 +0200

    almost done

--- cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/ops/rem.scala
+++ cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/ops/rem.scala
@@ -1,17 +1,7 @@
 package io.computenode.cyfra.core.expression.ops
 
-import io.computenode.cyfra.core.expression.*
-import io.computenode.cyfra.core.expression.Value.map
 import io.computenode.cyfra.core.expression.types.*
-import io.computenode.cyfra.core.expression.types.given
-import io.computenode.cyfra.core.expression.{Operator, Value}
-
-import scala.annotation.targetName
-
-// Matrix * Scalar
-extension [T <: FloatType: Value, M <: Mat[T]: Value](mat: M)
-  @targetName("matrixTimesScalar")
-  def *(scalar: T): M = Value.map(Operator.MatrixTimesScalar)(mat, scalar)
+import io.computenode.cyfra.core.expression.*
 
 def select[T: Value](cond: Bool, obj1: T, obj2: T): T = Value.map(Operator.Select)(cond, obj1, obj2)
 
