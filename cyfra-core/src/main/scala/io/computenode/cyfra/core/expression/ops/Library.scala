be305368548c05a4fd5b481c7b40c0095a4f13ff
MarconZet <25779550+MarconZet@users.noreply.github.com>
2026-04-13 13:56:25 +0200

    ack

---
+++ cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/ops/Library.scala
@@ -0,0 +1,9 @@
+package io.computenode.cyfra.core.expression.ops
+
+import io.computenode.cyfra.core.expression.types.*
+import io.computenode.cyfra.core.expression.*
+
+object Library:
+  def select[T: Value](cond: Bool, obj1: T, obj2: T): T = Value.map(Operator.Select)(cond, obj1, obj2)
+
+  def select[V <: Vec[Bool]: Value, T <: Vec[?]: Value](cond: V, obj1: T, obj2: T): T = Value.map(Operator.Select)(cond, obj1, obj2)
--- cyfra-core/src/main/scala/io/computenode/cyfra/core/expression/ops/rem.scala
+++
@@ -1,8 +0,0 @@
-package io.computenode.cyfra.core.expression.ops
-
-import io.computenode.cyfra.core.expression.types.*
-import io.computenode.cyfra.core.expression.*
-
-def select[T: Value](cond: Bool, obj1: T, obj2: T): T = Value.map(Operator.Select)(cond, obj1, obj2)
-
-def select[V <: Vec[Bool]: Value, T <: Vec[?]: Value](cond: V, obj1: T, obj2: T): T = Value.map(Operator.Select)(cond, obj1, obj2)
